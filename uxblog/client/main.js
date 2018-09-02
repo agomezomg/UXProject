7b6b69b36a3b641a7ade93d92cf6d2d206bffd73
agomezomg <agomez98@unitec.edu>
2018-09-02 17:05:02 -0600

    not important

--- uxblog/client/main.js
+++ uxblog/client/main.js
@@ -6,8 +6,8 @@ import App from '../imports/ui/App.js';
  
 
 Meteor.startup(() => {
-
-  render(<App />, document.getElementById('render-target'));
-
+	const Posts = new Mongo.Collection('posts');
+	Posts.insert({ title: 'Hello world', body: 'First post' });
+	render(<App />, document.getElementById('render-target'));
 });
 
