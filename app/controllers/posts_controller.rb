5bf5d7f371699de972e2cf81e4ba236dfc262b80
Mayra Salazar <mayra.salazar@agilesolutions.com>
2018-09-24 18:07:13 -0600

    this

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -70,6 +70,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title,:description,:theme,:photourl, :user_id).merge(user: current_user)
+      params.require(:post).permit(:title,:description,:theme,:photourl, :user_id)
     end
 end
